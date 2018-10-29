47c24b176c9e4235cef83ca4ab5919e8b2225b8d
Blas Castellano Moreno <blas.castellano@splashthat.com>
2018-10-29 23:43:03 +0100

    Add todo list action

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentDidMount() {
+    firebase
+      .firestore()
+      .collection('todos')
+      .onSnapshot((querySnapshot) => {
+        let newList = []
+        querySnapshot.forEach(function(doc) {
+          newList.push(doc.data().text)
+        })
+
+        this.setState({
+          todos: newList
+        })
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Todo list
           </p>
+          <ul>
+            {this.state.todos.map((item) => {
+              return (
+                <li>{item}</li>
+              )
+            })}
+          </ul>
           <a
             className="App-link"
             href="https://reactjs.org"

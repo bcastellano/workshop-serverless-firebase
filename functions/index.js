c7ad3a17d17de47bc815c74725bab1eadf02965c
Blas Castellano Moreno <blas.castellano@splashthat.com>
2018-11-12 23:30:00 -0500

    Add http function to add todo

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
+admin.initializeApp()
+const db = admin.firestore()
+db.settings({ timestampsInSnapshots: true})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.addTodo = functions.https.onRequest((request, response) => {
+  let todo = request.query.todo
+
+  db.collection('todos')
+    .add({text: todo})
+    .then(function () {
+        response.json({message: 'Document added successfully'})
+    })
+    .catch(function (error) {
+        console.error('Error removing document: ', error)
+        response.json({message: 'Document added successfully'})
+    })
+
+
+})
